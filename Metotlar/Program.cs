7ac5d84a19c49d0a35be9191c40c3e16ff37dd64
Zeynep Sağlam <zeynepsaglam1903@gmail.com>
2021-01-17 00:07:35 +0300

    KampIntro_Ders3

---
+++ Metotlar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+
+            string[] meyveler = new string[] { };
+
+            Products urun1 = new Products();
+            urun1.Adi = "Elma";
+            urun1.Fiyatı = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Products urun2 = new Products();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyatı = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Products[] urunler = new Products[] { urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyatı);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------------------");
+            }
+
+
+            Console.WriteLine("------------------Metotlar------------------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+           
+
+
+        }
+    }
+}
+//Dont repeat yourself = Dry - Clean Code - Best Practice
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        public void Ekle(Products urun1) 
+        {
+            Console.WriteLine("Sepete eklendi:" + urun1.Adi);
+
+        }
+
+    }
+}
